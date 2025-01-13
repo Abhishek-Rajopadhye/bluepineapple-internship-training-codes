df003bde1757cb7381300bb628bed11f42d4ed0b
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-01-13 11:41:06 +0530

    Day6 exercise 1

---
+++ Day6/library.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+class Library():
+    def __init__(self):
+        self.sqliteConnector = sqlite3.connect("library.db")
+        self.sqlCursor = self.sqliteConnector.cursor()
+        self.books = []
+        query = "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='books';"
+        self.sqlCursor.execute(query)
+        if(self.sqlCursor.fetchone()[0] == 1):
+            self.sqlCursor.execute("SELECT * FROM 'books';")
+            result =  self.sqlCursor.fetchall()
+            for r in result:
+                self.books.append({"title": r[1], "author": r[2], "copies": r[3]})
+        else:
+            self.sqlCursor.execute("CREATE TABLE 'books' (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, author TEXT NOT NULL, copies INTEGER NOT NULL);")
+            self.sqliteConnector.commit()
+
+    def addBook(self, title, author, copies):
+        self.sqlCursor.execute("INSERT INTO 'books' (title, author, copies) VALUES (?, ?, ?);", (title, author, copies))
+        self.sqliteConnector.commit()
+        self.books.append({"title": title, "author": author, "copies": copies})
+
+    def deleteBook(self, title):
+        for book in self.books:
+            if book['title'] == title:
+                self.sqlCursor.execute("DELETE FROM 'books' WHERE title = ?;", (title,))
+                self.sqliteConnector.commit()
+                self.books.remove(book)
+                return True
+        return False
+
+    def getBooks(self):
+        return [book for book in self.books]
+
+    def getBook(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        return None
+
+    def allocateBook(self, title):
+        for book in self.books:
+            if book['title'] == title:
+                book['copies'] -= 1
+                self.sqlCursor.execute("UPDATE books SET copies = ? WHERE title = ?;", (book['copies'], title))
+                self.sqliteConnector.commit()
+                return True
+        return False
+
+    def deallocateBook(self, title):
+        for book in self.books:
+            if book['title'] == title:
+                book['copies'] += 1
+                self.sqlCursor.execute("UPDATE books SET copies = ? WHERE title = ?;", (book['copies'], title))
+                self.sqliteConnector.commit()
+                return True
+        return False
---
+++ Day6/library_management.py
@@ -0,0 +1,61 @@
+import library
+
+def printMenu():
+    print("1. Add Book")
+    print("2. Display Books")
+    print("3. Delete Book")
+    print("4. Issue Book")
+    print("5. Return Book")
+    print("6. Search Book")
+    print("7. Exit")
+
+def printBook(book):
+    print("---------------------------------------")
+    print("Title: ", book["title"])
+    print("Author: ", book["author"])
+    print("Copies: ", book["copies"])
+    print("---------------------------------------")
+
+def printBooks(books):
+    for book in books:
+        printBook(book)
+
+printMenu()
+choice = int(input("Enter your choice: "))
+lib = library.Library()
+while choice != 7:
+    if choice == 1:
+        title = input("Enter title: ")
+        author = input("Enter author: ")
+        copies = int(input("Enter copies: "))
+        lib.addBook(title, author, copies)
+    elif choice == 2:
+        printBooks(lib.getBooks())
+    elif choice == 3:
+        title = input("Enter title: ")
+        if lib.deleteBook(title):
+            print("Book deleted")
+        else:
+            print("Book not found")
+    elif choice == 4:
+        title = input("Enter title: ")
+        if lib.allocateBook(title):
+            print("Book issued")
+        else:
+            print("Book not available")
+    elif choice == 5:
+        title = input("Enter title: ")
+        if lib.deallocateBook(title):
+            print("Book returned")
+        else:
+            print("Book not found")
+    elif choice == 6:
+        title = input("Enter title: ")
+        book = lib.getBook(title)
+        if book:
+            printBook(book)
+        else:
+            print("Book not found")
+
+    printMenu()
+    choice = int(input("Enter your choice: "))
